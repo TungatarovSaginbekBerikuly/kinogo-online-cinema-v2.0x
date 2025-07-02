75bf6dede8c0cf9adba994b97b9f64e03d3cc10e
TungatarovSaginbekBerikuly <tungatarov.man@gmail.com>
2025-07-02 12:33:45 +0500

    Добавил миграции и модели кино-схемы

---
+++ database/migrations/2025_07_02_065053_create_films_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('alias')->unique();
+            $table->string('director');
+            $table->foreignId('release_year_id')
+                  ->constrained('release_years')
+                  ->cascadeOnDelete();                             
+            $table->string('translator')->nullable();
+            $table->integer('duration');
+            $table->text('description');
+            $table->text('player');
+            $table->boolean('is_recommended');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('films');
+    }
+};
